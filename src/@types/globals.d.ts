33e40081e98c79b2c355bbcb3beab1db7d248a5b
Dean <deanly88@gmail.com>
2019-08-22 11:04:11 +0900

    Adding global funcstions

--- src/@types/globals.d.ts
+++ src/@types/globals.d.ts
@@ -1,6 +1,29 @@
+/**
+ * global declares
+ */
+
+interface ErrorExt extends Error {
+    name: string;
+    code: string | number;
+    message: string;
+    stack?: string;
+}
+
+declare interface ErrorExtConstructor {
+    new(message: string, code: string | number): ErrorExt;
+    readonly prototype: ErrorExt;
+}
+
+declare interface Array<T> {
+    asyncForEach: (callbackfn: (value: T, index: number, array: T[]) => Promise<void>) => Promise<void>;
+}
 
 declare namespace NodeJS {
     interface Global {
+        ErrorExt: ErrorExtConstructor;
+        setTimeoutPromise: (fn: Function, ms: number) => Promise<void>;
+        handleError: (e: Error | ErrorExt) => void;
+
         host: string;
         http_port: number;
         socket_port: number;
